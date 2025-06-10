33ca54b3fcd379a507019e99a91847cfd7b03bf5
ItoYama <daxiangy147@gmail.com>
2025-06-10 11:41:18 +0900

    robot_function.inoのサンプルコードを修正 (左、右移動を加えた)

--- GR-ROSE/robot_function.ino
+++ GR-ROSE/robot_function.ino
@@ -26,7 +26,7 @@ void krs_setposition(IcsServo* servo, float angle){
 
 void setup() {
   Serial.begin(9600);
-  ICS1.begin(1250000);//サーボの通信速度を1250000に設定しているためカッコ内を変える
+  ICS1.begin(1250000);
   ICS3.begin(1250000);
   ICS4.begin(1250000);
   //回収装置セットアップ
@@ -36,7 +36,7 @@ void setup() {
     delay(1);
     C_servo[i].setSpeed(60);
     delay(1);
-    krs_setposition(&C_servo[i], 45);//回収装置の初期角度は45度．後々で調整必要かも
+    krs_setposition(&C_servo[i], 45);//回収装置の初期角度は45度．
     delay(1);
   }
   //前輪セットアップ
@@ -63,7 +63,7 @@ void setup() {
   R_servo[2].attach(ICS4, 2+1); 
   R_servo[2].setStretch(30);
   delay(1);
-  R_servo[2].setSpeed(30); //少しゆっくりにしてみた
+  R_servo[2].setSpeed(30);
   delay(1);
   krs_setposition(&R_servo[2], -2);//かごの初期角度は-2度程度
   delay(1);
@@ -72,8 +72,8 @@ void setup() {
 }
 
 void loop() {
-  //回収装置
-  krs_setposition(&C_servo[4], 45); //一応入れている
+  //回収動作
+  krs_setposition(&C_servo[4], 45);
   krs_setposition(&C_servo[3], 45); //セットアップでこの角度になっているはず
   delay(500);
   krs_setposition(&C_servo[4], 0);
@@ -98,29 +98,63 @@ void loop() {
   krs_setposition(&R_servo[1], 0);
   delay(3000);
 
-  //斜め移動
-  krs_setposition(&F_servo[0], 25); //-90~90度まで．それ以上はコードが断線する危険があるのでダメ．
-  krs_setposition(&R_servo[0], 25);
+  //後退
+  krs_setposition(&F_servo[0], 0);
+  krs_setposition(&R_servo[0], 0);
   delay(2000);
-  krs_setposition(&F_servo[1], 30);
-  krs_setposition(&R_servo[1], 30);
-  delay(3000);
-  krs_setposition(&F_servo[1], 0);
-  krs_setposition(&R_servo[1], 0);
-  delay(2000);
-
-  //旋回的な何か．サーボIDで楽しようとしたらこうなった，ID変える作業めんどくさい．
-  krs_setposition(&F_servo[0], 90);
-  krs_setposition(&R_servo[0], 90);
-  delay(2000);
-  krs_setposition(&F_servo[1], 20);
-  krs_setposition(&R_servo[1], -20);
-  delay(3000);
+  krs_setposition(&F_servo[1], -100); //0で停止．135まで．マイナス付けると逆回転
+  krs_setposition(&R_servo[1], -100); //セットスピードとセットポジションの値で速度がきまるらしいが，純粋な比例かどうか不明
+  delay(3000); //回転時間
   krs_setposition(&F_servo[1], 0);
   krs_setposition(&R_servo[1], 0);
   delay(3000);
 
-  //かご
+
+//右移動
+  krs_setposition(&F_servo[0], 10);
+  krs_setposition(&R_servo[0], 10);
+  delay(2000);
+  krs_setposition(&F_servo[1], 100); //0で停止．135まで．マイナス付けると逆回転
+  krs_setposition(&R_servo[1], 100); //セットスピードとセットポジションの値で速度がきまるらしいが，純粋な比例かどうか不明
+  delay(3000); //回転時間
+  krs_setposition(&F_servo[1], 0);
+  krs_setposition(&R_servo[1], 0);
+  delay(3000);
+
+//左移動
+  krs_setposition(&F_servo[0], -10);
+  krs_setposition(&R_servo[0], -10);
+  delay(2000);
+  krs_setposition(&F_servo[1], 100); //0で停止．135まで．マイナス付けると逆回転
+  krs_setposition(&R_servo[1], 100); //セットスピードとセットポジションの値で速度がきまるらしいが，純粋な比例かどうか不明
+  delay(3000); //回転時間
+  krs_setposition(&F_servo[1], 0);
+  krs_setposition(&R_servo[1], 0);
+  delay(3000);
+
+  // //斜め移動
+  // krs_setposition(&F_servo[0], 25); //-90~90度まで．それ以上はコードが断線する危険があるのでダメ．
+  // krs_setposition(&R_servo[0], 25);
+  // delay(2000);
+  // krs_setposition(&F_servo[1], 30);
+  // krs_setposition(&R_servo[1], 30);
+  // delay(3000);
+  // krs_setposition(&F_servo[1], 0);
+  // krs_setposition(&R_servo[1], 0);
+  // delay(2000);
+
+  // //旋回的な何か．サーボIDで楽しようとしたらこうなった，ID変える作業めんどくさい．
+  // krs_setposition(&F_servo[0], 90);
+  // krs_setposition(&R_servo[0], 90);
+  // delay(2000);
+  // krs_setposition(&F_servo[1], 20);
+  // krs_setposition(&R_servo[1], -20);
+  // delay(3000);
+  // krs_setposition(&F_servo[1], 0);
+  // krs_setposition(&R_servo[1], 0);
+  // delay(3000);
+
+  //かごを傾け、ボールを落とす動作
   krs_setposition(&R_servo[2], 90);
   delay(4000);
   krs_setposition(&R_servo[2], -2);
